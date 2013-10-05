86d3d206e8b31766989579c55029b322bf689eda
Leon Yao <leonxyao@gmail.com>
2013-10-05 16:20:01 -0700

    sigh

--- web.js
+++ web.js
@@ -1,11 +1,26 @@
-var express = require("express");
-var app = express();
+/*var express = require("express");
+
+var app = express.createServer();
 var fs = require('fs');
 var index =fs.readFileSync("/nodejs/index.html");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send(index);
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});*/
+
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
