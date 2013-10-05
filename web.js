4174cbd20d31ccca02bafadfaf21963de46c2b74
Leon Yao <leonxyao@gmail.com>
2013-10-05 16:41:44 -0700

    help?

--- web.js
+++ web.js
@@ -2,28 +2,17 @@ var express = require("express");
 
 var app = express.createServer();
 var fs = require('fs');
-var index =fs.readFileSync("/nodejs/index.html");
+var index =fs.readFileSync("~/Documents/hackMIT/nodejs/index.html");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.render(index);
 });
 
+//app.listen(8080);
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-/*var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});*/
