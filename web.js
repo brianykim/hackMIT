0b5d81de75e0cfd9ca9f729513b83ae9690b68d3
Leon Yao <leonxyao@gmail.com>
2013-10-05 16:22:33 -0700

    hello

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-/*var express = require("express");
+var express = require("express");
 
 var app = express.createServer();
 var fs = require('fs');
@@ -13,9 +13,9 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});*/
+});
 
-var express = require("express");
+/*var express = require("express");
 var app = express();
 app.use(express.logger());
 
@@ -26,4 +26,4 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
