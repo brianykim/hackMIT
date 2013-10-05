d0d268f36b1d2344d7e477442045057563f9756d
Leon Yao <leonxyao@gmail.com>
2013-10-05 13:11:18 -0700

    init

--- nodejs/web.js
+++ nodejs/web.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var app = express();
+app.use(express.logger());
 
-var http = require('http');
+app.get('/', function(request,response)
+{
+  //Login here
+  //send the login data to firebase
+  response.send("Hello World");
+});
