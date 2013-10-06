b7c2fd8b20da2bb862cbcf57c9620b6f00828896
Leon Yao <leonxyao@gmail.com>
2013-10-05 23:57:39 -0700

    sigh

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/*var express = require("express");
 
 var app = express.createServer();
 var fs = require('fs');
@@ -6,7 +6,7 @@ var index =fs.readFileSync("~/Documents/hackMIT/nodejs/index.html");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.render(index);
+  response.end(index);
 });
 
 //app.listen(8080);
@@ -15,4 +15,17 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});*/
+
+var http = require('http'),
+    fs = require('fs');
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
