9066d2d308107a4e790fa04f42da6cae6e389b5a
Brian Kim <byk.briankim@gmail.com>
2013-10-06 05:35:52 -0700

    last updates for webpage where all the bullshit comes together

--- nodejs/script.js
+++ nodejs/script.js
@@ -4,19 +4,13 @@ $(document).ready(function(){
     });
     $('div ul li').mouseleave(function(){
         $(this).fadeTo('fast',1);
-    })
+    });
     $('#button1').mouseenter(function(){
         $(this).fadeTo('fast',1);
-    })
+    });
     $('#button1').mouseleave(function(){
         $(this).fadeTo('fast',0.5);
-    })
+    });
     //you can just use hover
 
-function populate(list){
-    for(var i = 0; i < list.length; i++){
-        var item = list[i];
-        $('#list').after('<li>' + item + '</li>');
-    }
-}
 });
